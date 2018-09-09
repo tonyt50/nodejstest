b283783c484dd7965c2ab493eb6e71da8f44449c
tony_t50 <tony_t50@hotmail.com>
2018-09-09 16:00:29 +0100

    test collaberation

--- testfunctions.js
+++ testfunctions.js
@@ -43,9 +43,9 @@ function test001(inputstring) {
       } else {
         maxoffset = offsets[offsetsindex] - 1;
       }
-//      possiblelengths["" + minoffset + maxoffset] = {
+      possiblelengths["" + minoffset + maxoffset] = {
       // TODO here check if valid, as dont check current multipleletterskey
-      possiblelengths[possiblelengthskey++] = {
+//      possiblelengths[possiblelengthskey++] = {
         min: minoffset,
         max: maxoffset,
         len: maxoffset - minoffset + 1

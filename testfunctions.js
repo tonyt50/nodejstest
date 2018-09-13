6b4d0a684e125127a7f4e268ca86dc156dd7976e
tony_t50 <tony_t50@hotmail.com>
2018-09-13 22:58:17 +0100

    starting to use functions as var

--- testfunctions.js
+++ testfunctions.js
@@ -1,9 +1,42 @@
-function test001(inputstring) {
-  console.log(inputstring, " : ", typeof inputstring);
-  if ((typeof inputstring === "string") === false) {
-    inputstring += "";
+var getpossiblelengths =
+function (multipleletters, lengthofstring) {
+  let possiblelengths = {};
+  let possiblelengthskey = 1;
+  let multipleletterskey = {};
+  let offsets = [];
+  let offsetsindex = 0;
+  let minoffset = 0;
+  let maxoffset = 0;
+  for (multipleletterskey in multipleletters) {
+    offsets = multipleletters[multipleletterskey];
+    offsetsindex = 0;
+    while (offsetsindex < offsets.length) {
+      if (offsetsindex === 0) {
+        minoffset = 0;
+      } else {
+        minoffset = offsets[offsetsindex - 1] + 1;
+      }
+      offsetsindex++;
+      if (offsetsindex === offsets.length) {
+        maxoffset = lengthofstring;
+      } else {
+        maxoffset = offsets[offsetsindex] - 1;
+      }
+      //possiblelengths["" + minoffset + maxoffset] = {
+        // comment for jed
+        // TODO here check if valid, as dont check current multipleletterskey
+      possiblelengths[possiblelengthskey++] = {
+        min: minoffset,
+        max: maxoffset,
+        len: maxoffset - minoffset + 1
+      };
+    }
   }
-  let lengthofstring = inputstring.length - 1;
+  return possiblelengths;
+}
+
+var getmultipleletters =
+function (inputstring) {
   let singleletters = {};
   let multipleletters = {};
 
@@ -20,40 +53,20 @@ function test001(inputstring) {
       }
     }
   }
+  return multipleletters;
+}
 
-  let multipleletterskey = {};
-  let offsets = [];
-  let offsetsindex = 0;
-  let possiblelengths = {};
-  let possiblelengthskey = 1;
-  let minoffset = 0;
-  let maxoffset = 0;
-  for (multipleletterskey in multipleletters) {
-    offsets = multipleletters[multipleletterskey];
-    offsetsindex = 0;
-    while (offsetsindex < offsets.length) {
-      if (offsetsindex === 0) {
-        minoffset = 0;
-      } else {
-        minoffset = offsets[offsetsindex - 1] + 1;
-      }
-      offsetsindex++;
-      if (offsetsindex === offsets.length) {
-        maxoffset = inputstring.length - 1;
-      } else {
-        maxoffset = offsets[offsetsindex] - 1;
-      }
-      possiblelengths["" + minoffset + maxoffset] = {
-        // comment for jed
-        // TODO here check if valid, as dont check current multipleletterskey
-//      possiblelengths[possiblelengthskey++] = {
-        min: minoffset,
-        max: maxoffset,
-        len: maxoffset - minoffset + 1
-      };
-    }
+function test001(inputstring) {
+  console.log(inputstring, " : ", typeof inputstring);
+  if ((typeof inputstring === "string") === false) {
+    inputstring += "";
   }
-  console.log(singleletters);
+  let lengthofstring = inputstring.length - 1;
+
+  let multipleletters = getmultipleletters(inputstring);
+
+  let possiblelengths = getpossiblelengths(multipleletters, inputstring.length - 1);
+
   console.log(multipleletters);
   console.log("possiblelengths < \n", possiblelengths, "\n  >");
 

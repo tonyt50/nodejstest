ec89ab33205a7ee600739d8d21ba1c72abec63c1
Antony Thompson <tonyt@antonys-mbp.home>
2018-10-07 13:22:43 +0100

    test debug

--- src/longestSubstring.test.ts
+++ src/longestSubstring.test.ts
@@ -1,30 +1,35 @@
 import longestSubstring from "./longestsubstring";
-interface TestString {
-  testNum: number;
-  value: string;
-  result: number;
-}
-const testStrings: TestString[] = [
-  {
-    testNum: 1,
-    value: "retested",
-    result: 4
-  },
-  {
-    testNum: 2,
-    value: "abba",
-    result: 2
-  },
-  {
-    testNum: 3,
-    value: "banana",
-    result: 3
-  }
-];
+// interface TestString {
+//   testNum: number;
+//   value: string;
+//   result: number;
+// }
 
-testStrings.forEach(({ result, testNum, value }) => {
-  test(`${value} returns ${result}`, () =>
-    expect(longestSubstring(value)).toBe(result));
-});
+// const testStrings: TestString[] = [
+//   {
+//     testNum: 1,
+//     value: "retested",
+//     result: 4
+//   },
+//   {
+//     testNum: 2,
+//     value: "abba",
+//     result: 2
+//   },
+//   {
+//     testNum: 3,
+//     value: "banana",
+//     result: 3
+//   }
+// ];
+
+// const runonetest = ({ testNum, value, result }: TestString) => {
+//   test(`${testNum} ${value} returns ${result}`, () =>
+//     expect(longestSubstring(value)).toBe(result));
+// };
+
+// testStrings.forEach(input => {
+//   runonetest(input);
+// });
 
 test("retested returns 4", () => expect(longestSubstring("retested")).toBe(4));
